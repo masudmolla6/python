add4346287fb353dc6d57eec36399ec20f59bac9
masudmolla6 <masudmolla2937@gmail.com>
2024-11-13 14:15:49 +0600

    complete while loop

--- loop.py
+++ loop.py
@@ -4,7 +4,33 @@
 # 2-for loop
 
 # while loop
-i=1
-while i <= 6:
-    print(i)
-    i=i+1
+# i=1
+# while i <= 6:
+#     print(i)
+#     i=i+1
+
+# break
+# x=1
+# while x<=6:
+#     print("Value of x", x)
+#     if x == 3:
+#         break
+#     x+=1
+
+# continue statement
+# x=1
+# while x < 10:
+#     x+=1
+#     if x==5:
+#         continue
+#     print(x)
+
+#  else Statement for while loop
+x=1
+while x < 6:
+    print(x)
+    x+=1
+else:
+    print("x is no longer then 6")
+
+
