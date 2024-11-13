cf5cb75353c40be43566830f3a54e2d964e2f988
masudmolla6 <masudmolla2937@gmail.com>
2024-11-13 18:25:43 +0600

    try to learn python for loop

---
+++ forLoop.py
@@ -0,0 +1,28 @@
+# for loop use in list
+# fruits=["apple", "mango", "orange"]
+#
+# for item in fruits:
+    # print(item)
+
+# for loop use in string
+# myName="Masud";
+# for word in myName:
+#     print(word)
+
+# break
+
+myList=[2,45,7,8,4,6,78,34]
+# for item in myList:
+#     print(item)
+#     if item==8:
+#         break
+
+for item in myList:
+    if item==8:
+        break
+    print(item)
+
+
+
+
+
