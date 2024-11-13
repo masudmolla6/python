5b78a2748d9236bc98d444dfac21ec4cf002d0d2
masudmolla6 <masudmolla2937@gmail.com>
2024-11-13 13:51:36 +0600

    complete conditional statement in python.

--- condition.py
+++ condition.py
@@ -19,6 +19,28 @@ c=23
 
 # print("a is greater than b") if a > b else print("a and b are equal") if a==b else print("b are greater than a");
 
+# and operator
+# if a < b and b > c:
+#     print("both condition are ture.")
 
-if a < b and b > c:
-    print("both condition are ture.")
+# or operator
+# if  a > b or b>c:
+#     print("first condition are false but secound condition are true");
+
+# not Operator
+
+# if not a>b:
+#     print("a is not greater then b");
+
+# nested if
+x=30
+# if x < 10:
+#      print("10 are less then x")
+# if x<20:
+#     print("20 are less then x")
+# else:
+#     print("x are greater then all value you are assign");
+
+
+# pass statement
+
