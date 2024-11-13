64028e4eeb81dfeddea5a8215df8cbc45e1df913
masudmolla6 <masudmolla2937@gmail.com>
2024-11-13 21:02:34 +0600

    complete loop list

--- forLoop.py
+++ forLoop.py
@@ -11,16 +11,40 @@
 
 # break
 
-myList=[2,45,7,8,4,6,78,34]
+# myList=[2,45,7,8,4,6,78,34]
 # for item in myList:
 #     print(item)
 #     if item==8:
 #         break
 
-for item in myList:
-    if item==8:
-        break
-    print(item)
+# for item in myList:
+#     if item==8:
+#         break
+#     print(item)
+
+# continue
+# for item in myList:
+#     if item==78:
+#         continue
+#     print(item)
+
+# rang function
+
+# for x in range(6):
+#     if x==4:break
+#     print(x)
+# else:
+#     print("Finally Finished.")
+
+friends=["Masud", "Karim", "Rahim"]
+fruits=["apple", "orange", "banana"]
+# for friend in friends:
+#     for item in fruits:
+#         print(friend, item)
+
+# pass
+for x in fruits:
+    pass
 
 
 
---
+++ loopList.py
@@ -0,0 +1,16 @@
+from binaryType import result1
+
+numbers =[1,2,3,4,5,6,7,8,9]
+# for number in numbers:
+#     print(number)
+
+
+# Loop Through the Index Numbers
+# for x in range(len(thislist)):
+#     print(x)
+
+thislist = ["apple", "banana", "cherry"]
+x=0
+while x < len(thislist):
+    print(thislist[x])
+    x=x+1
