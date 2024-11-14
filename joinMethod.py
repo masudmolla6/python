b377a839fc5b32d836882495439784de0edad296
masudmolla6 <masudmolla2937@gmail.com>
2024-11-14 17:40:08 +0600

    complete list join

---
+++ joinMethod.py
@@ -0,0 +1,22 @@
+# join in list
+
+# 1-way(+)
+
+# list1=[2,3,4,5,6,7]
+# list2=[6,4,5,6]
+# myList=list1+list2
+# print(myList)
+
+# 2-way(loop-append)
+# for x in list1:
+#     list2.append(x)
+# print(list2)
+
+# 3-way(extend method)
+# list2.extend(list1)
+# print(list2)
+
+# test
+fruits=["apple", "Orange", "mango"]
+fruits[0]="lemon"
+print(fruits);
