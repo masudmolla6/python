3b9111c198c24c9e2f28b065b5d01ca434cc789f
masudmolla6 <masudmolla2937@gmail.com>
2024-11-15 11:21:14 +0600

    complete unpack Tuple

--- tuple.py
+++ tuple.py
@@ -10,14 +10,37 @@ myNumbers=tuple((2,3,4,5))
 # print(myNumbers[-3])
 
 # range
-thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 
 # print(thistuple[:4])
 # print(thistuple[2:])
 # print(thistuple[-4:-1])
 
 
-if "cherr" in thistuple:
-    print("apple is exits in the tuple")
-else:
-    print("apple is not exits in the tuple")
+# if "cherr" in thistuple:
+#     print("apple is exits in the tuple")
+# else:
+#     print("apple is not exits in the tuple")
+
+# thistuple = ("apple", "banana", "cherry")
+# converTupleIntoList=list(thistuple)
+# # converTupleIntoList[2]="melon"
+# converTupleIntoList.append("Mango")
+# againConverintoTuple=tuple(converTupleIntoList);
+# print(againConverintoTuple)
+
+# x=[2,34]
+# y=(67,)
+# x+=y
+# print(x)
+
+# remove item in tuple
+myNumbers=(3,4,5,6,7)
+# x=list(myNumbers)
+# x.remove(5);
+# print(tuple(x))
+
+# delete tuple
+del myNumbers
+print(myNumbers)
+
---
+++ unpackTuple.py
@@ -0,0 +1,6 @@
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, *yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
