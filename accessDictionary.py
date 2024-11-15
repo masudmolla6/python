e1c7fa4f8f9d06f3724f9f90a66ea27323784187
masudmolla6 <masudmolla2937@gmail.com>
2024-11-15 20:28:15 +0600

    access dictionary item

---
+++ accessDictionary.py
@@ -0,0 +1,29 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+# model=thisdict["model"]
+# model=thisdict.get("model")
+# x=thisdict.keys()
+# print(x)
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+# x=car.keys()
+# car["color"]="white"
+# print(x)
+
+# values=car.values()
+# car["year"]=2020
+# car["Owner"]="Masud Rana"
+# print(values)
+
+# print(car.items())
+
+if "model" in car:
+    print("Yes, 'model' is one of the keys in the thisdict dictionary")
