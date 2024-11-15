df79b8d581508b24b1d691272c7a72c407870039
masudmolla6 <masudmolla2937@gmail.com>
2024-11-15 18:41:34 +0600

    complete set item remove

---
+++ removeSetItem.py
@@ -0,0 +1,12 @@
+# remove in set
+fruits={"banana", "apple", "cherry"}
+fruits.remove("apple")
+fruits.discard("cherryvb")
+fruits.pop()
+fruits.clear()
+del fruits
+print(fruits)
+
+# loop in set
+for x in fruits:
+    print(x)
