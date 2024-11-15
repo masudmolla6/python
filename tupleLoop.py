eda7ff098336c280d57cc3d5121122796df98e66
masudmolla6 <masudmolla2937@gmail.com>
2024-11-15 12:03:47 +0600

    complete tuple method

---
+++ tupleLoop.py
@@ -0,0 +1,20 @@
+numbers=(1,2,4,6,7,8)
+# for number in numbers:
+#     print(number)
+
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+i=0
+# while i<len(numbers):
+#     print(numbers[i])
+#     i+=1
+
+# tuple join
+num1=(1,2,5,6,78,9)
+# num2=(4,5,6,7,8,9)
+# newNumber=num1+num2
+# print(newNumber)
+
+newNumbers=num1*2
+print(newNumbers)
---
+++ tupleMethod.py
@@ -0,0 +1,9 @@
+# Count
+numbers=(2,3,4,5,6,78,8,9,6,5,4,3,2, 356,3,56,3,3)
+x=numbers.count(3)
+# print(x)
+
+# index
+y=numbers.index(6)
+print(y)
+
