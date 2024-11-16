3b2b7cbc49906a19f16f9d9c3dee55898a28890a
masudmolla6 <masudmolla2937@gmail.com>
2024-11-16 19:05:04 +0600

    complete python array

---
+++ array.py
@@ -0,0 +1,11 @@
+cars=["VMW", "Volvo", "Ford"]
+cars[1]="toyota"
+# print(cars[1])
+# print(len(cars))
+
+# cars.append("cng")
+# cars.pop()
+# cars.pop(0)
+cars.remove("Ford")
+for car in cars:
+    print(car)
