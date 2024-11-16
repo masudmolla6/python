002b6514fa9dfff86e8d86655b3399c37b0e4f17
masudmolla6 <masudmolla2937@gmail.com>
2024-11-16 09:35:09 +0600

    learning python function

---
+++ function.py
@@ -0,0 +1,10 @@
+# def my_name(firstName, lastName):
+#     print("My Name is ",firstName +" "+lastName)
+#
+#
+# my_name("Masud", "Rana")
+
+def my_function(*kids):
+    print("The Youngest child is", kids[0])
+
+my_function("Rahim", "Karim", "Kadir")
